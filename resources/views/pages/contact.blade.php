2beb7d5d377974f7fb166bb2006f66916e10d145
krutika <krr39@njit.edu>
2018-10-16 22:37:23 -0400

    project finished

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.default')
+
+@section('content')
+    <div clas="card mt-3 pl-2 pr-2">
+        <div class="card-title"><h1>Contact</h1></div>
+
+        <div class="card-body">
+
+            <form action="/contact" method="post">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                </div>
+
+                <div class="form-group">
+                    <label for="body">Message</label>
+                    <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-primary mb-2">Submit</button>
+            </form>
+        </div>
+    </div>
+
+@endsection
